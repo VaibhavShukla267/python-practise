e603ec038ef87f5261816b7957e1643ef1bd150a
Vaibhav Shukla <shuklavaibhav267@gmail.com>
2022-12-08 16:13:39 +0530

    Update main.py

--- project/tic tac toe/main.py
+++ project/tic tac toe/main.py
@@ -16,7 +16,7 @@ def printBoard(xState,zState):
     print(f"{six}    |   {seven}      |  {eight} ")
     
 def checkWin(xState,zState):
-    wins=[{0,1,2} , {3,4,5} , {6,7,8} ,{0,3,6}, {1,4,7} , {3,6,9} , {0,4,8} , {2,4,6}]
+    wins=[[0,1,2] , [3,4,5] , [6,7,8] ,[0,3,6], [1,4,7] , [3,6,9] , [0,4,8] , [2,4,6]]
     for win in wins:
             if(sum(xState[win[0]], xState[win[1]] , xState[win[2]])==3):
                 print("X won the match" )
